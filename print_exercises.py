c43e590ea28d837d11945d4117ddbf59f7fbd18a
riltho1 <100237088+riltho1@users.noreply.github.com>
2022-02-24 10:59:11 +1300

    Print Exercises

---
+++ print_exercises.py
@@ -0,0 +1,7 @@
+print("Riley")
+print("Mrs White")
+print("King's Hgh School")
+print("14")
+print(28*24)
+print(2022+25)
+print(14+100)
--- test.py
+++ test.py
@@ -1,2 +1,10 @@
 print("Hello World")
 print("Silly little boy")
+print("9+10")
+print(9+10)
+print("Chocolate Nuts")
+print("\n")
+
+age = input("How old are you")
+
+print(f"You are {age} years old.")
