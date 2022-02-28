d832fbb6c44590cfe295aa0d248330676998e271
riltho1 <100237088+riltho1@users.noreply.github.com>
2022-02-28 13:23:56 +1300

    Updated_Data_Types

--- data_types.py
+++ data_types.py
@@ -1,1 +1,20 @@
+#4.4 Excercises
+"""
 print("It ain't me, it ain't me\nIt ain't no millionaire's son,\nno, no It ain't me, it ain't me\nI ain't no fortunate one")
+print("I'm gonna put some dirt in your eye.")
+print("A string is a print statement where the computer prints what you have wrote. It can be any characters and any  amount of them.")
+print("An integer is a whole number they can be used together with floatsand preform other operations together.")
+print("A float is a decimal number that can be used together with integers to preform other operations.")
+print(543/3)
+print(543//3)
+print(543%3)
+"""
+#Print out these answers
+print(1000%9)
+print(434//10)
+print(3%2)
+print(5%5)
+print(555/2)
+print(123/2)
+print(123//2)
+print(123%2)
