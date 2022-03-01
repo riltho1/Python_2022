409e38e77b0439b84f9ef1a4b2adec0bb4fee201
riltho1 <100237088+riltho1@users.noreply.github.com>
2022-03-01 14:57:58 +1300

    Data_Types_Finished

--- data_types.py
+++ data_types.py
@@ -1,20 +1,40 @@
 #4.4 Excercises
 """
-print("It ain't me, it ain't me\nIt ain't no millionaire's son,\nno, no It ain't me, it ain't me\nI ain't no fortunate one")
-print("I'm gonna put some dirt in your eye.")
-print("A string is a print statement where the computer prints what you have wrote. It can be any characters and any  amount of them.")
-print("An integer is a whole number they can be used together with floatsand preform other operations together.")
-print("A float is a decimal number that can be used together with integers to preform other operations.")
-print(543/3)
-print(543//3)
-print(543%3)
-"""
+print("It ain't me, it ain't me\nIt ain't no millionaire's son,\nno, no It ain't me, it ain't me\nI ain't no fortunate one") #1 
+print("I'm gonna put some dirt in your eye.") #2 
+print("A string is a print statement where the computer prints what you have wrote. It can be any characters and any  amount of them.") #3 
+print("An integer is a whole number they can be used together with floatsand preform other operations together.") #4 
+print("A float is a decimal number that can be used together with integers to preform other operations.") #5 
+print(543/3) #6 
+print(543//3) #7 
+print(543%3) #8 
+
 #Print out these answers
-print(1000%9)
-print(434//10)
-print(3%2)
-print(5%5)
-print(555/2)
-print(123/2)
-print(123//2)
-print(123%2)
+print(1000%9) #9
+print(434//10) #10
+print(3%2) #11
+print(5%5) #12
+print(555/2) #13
+print(123/2) #14
+print(123//2) #15
+print(123%2) #16
+print(764%2) #17 
+print(165//2) #18
+
+#Integers/Floats
+print(5.2+6.7) #19 
+print(3+5) #20 
+print(3-1.3) #21 
+print(23.3-7) #22
+print(4//2) #23 
+print(3*14.2) #24 
+print(7*8) #25 
+print("Riley " *10) #26 
+print("Riley\n" *10) #27
+"""
+
+#Strings and Type Conversations
+print("5+10=" + str(5+10)) 
+print("15/2=" + str(15/2))
+print("30/2*5=" + str(30/2*5))
+print("30/2*5=" + str(30//2*5))
