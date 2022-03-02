9bbbc8cf467237b0611d8bca7301534ecc640904
riltho1 <100237088+riltho1@users.noreply.github.com>
2022-03-03 09:55:06 +1300

    Update excercises-variable_practice.py

--- excercises-variable_practice.py
+++ excercises-variable_practice.py
@@ -1,2 +1,32 @@
 #Excercises - Variable Practice
-print("Hello World")
+"""
+player_lives = 3 #1
+player_name = "Pie" #2 
+player_money = 420 #3 
+
+#Variable Inputs
+phone_number = input("What is your phone number? ") #4
+user_name = input("What is your name? ") #5
+user_age = input("How old are you? ") #6 
+current_year = input("What year is it? ") #7
+print("\n")
+
+#Print Variables
+print(f"Your name is {user_name}") #8 
+print(f"You are {user_age} years old") #9
+print(f"The current year is {current_year}") #10
+print("\n")
+
+#More Stored Variables
+user_surname = input("What is your last name? ") #11
+user_school = input("What school do you go too? ") #12
+user_food = input("Whats your favorite food? ") #13
+pet_name = input("What is your pets name? ") #14
+"""
+#Tracking Varibles
+chocolate_blocks = 5 #15
+player_score = 200 #16
+player_highscore = 300 #17
+player_laps = 3%2 #18
+player_ammo = 23%30 #19
+player_gear = 
