b3818b690179649b9cf503437b13411d58682216
riltho1 <100237088+riltho1@users.noreply.github.com>
2022-03-07 12:25:04 +1300

    Update input_exercises.py

--- input_exercises.py
+++ input_exercises.py
@@ -3,7 +3,7 @@
 input("Press any key, press enter. ") #1
 input("Type your name and enter. ") #2
 input("Type your age and enter. ") #3
-"""
+print("\n")
 
 #Stored Inputs
 user_name = input("Enter your name. ") #4
@@ -12,3 +12,16 @@ favourite_movie = input("What is your favourite movie? ") #6
 book_name = input("What's your favourite book? ") #7
 user_adjective = input("Name an adjective. ") #8
 user_noun = input("Name a noun. ") #9
+user_verb = input("Name a verb. ") #10
+print("\n")
+
+print(f"Hello there {user_name}. ")
+print(f"You are {user_age}. ")
+print(f"{favourite_movie} is good.")
+print(f"{book_name} is bad though.")
+print(f"{user_adjective} is a very bad describing word.")
+print(f"{user_noun} is a good word. ")
+print(f"{user_verb} is a good word. ") #11
+"""
+#Conversion to Integers
+user_age = input(f"How old are you?\n") #13
