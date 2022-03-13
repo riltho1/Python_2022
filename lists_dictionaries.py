19d096ff4fd4fdf0d9067b556460bbe6322a0a64
riltho1 <100237088+riltho1@users.noreply.github.com>
2022-03-14 10:56:12 +1300

    Update lists_dictionaries.py

--- lists_dictionaries.py
+++ lists_dictionaries.py
@@ -1,5 +1,12 @@
 fibbonachi = 0,1,1,2,3,5,8,13,21,34
 print(f"The first 10 numbers of the Fibonacci sequence are {fibbonachi}") #1
 
-friuts = ["Bannanas, Apples, Strawberries, Oranges"]
-print(f"My favourite friuts are {friuts}")
+friuts = ['Bannanas', 'Apples', 'Strawberries', 'Oranges']
+print(f"My favourite friuts are {','.join(friuts)}") #2
+
+youtubers = ["NoahsNoah", "Jev", "Sidemen", "WILDCAT"]
+print(f"My favorite youtubers are {','.join(youtubers)}") #3
+
+favourite_songs = []
+favourite_songs.append("Clash - Dave")
+print(f"My favourite songs are {','.join(favourite_songs)}")
