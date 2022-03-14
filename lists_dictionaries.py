279cbf4baf09b9c910087227aa04cd0b87f05a4e
riltho1 <100237088+riltho1@users.noreply.github.com>
2022-03-15 12:24:58 +1300

    Update lists_dictionaries.py

--- lists_dictionaries.py
+++ lists_dictionaries.py
@@ -1,12 +1,52 @@
+"""
 fibbonachi = 0,1,1,2,3,5,8,13,21,34
 print(f"The first 10 numbers of the Fibonacci sequence are {fibbonachi}") #1
+print("\n")
 
-friuts = ['Bannanas', 'Apples', 'Strawberries', 'Oranges']
+friuts = ['Bannanas', ' Apples', ' Strawberries', ' Oranges']
 print(f"My favourite friuts are {','.join(friuts)}") #2
+print("\n")
 
-youtubers = ["NoahsNoah", "Jev", "Sidemen", "WILDCAT"]
+youtubers = ["NoahsNoah", " Jev", " Sidemen", " WILDCAT"]
 print(f"My favorite youtubers are {','.join(youtubers)}") #3
+print("\n")
 
 favourite_songs = []
-favourite_songs.append("Clash - Dave")
-print(f"My favourite songs are {','.join(favourite_songs)}")
+favourite_songs.append("Clash")
+favourite_songs.append(" Sucker for Pain")
+favourite_songs.append(" Hedwig's Theme")
+favourite_songs.append(" Friday")
+favourite_songs.append(" Crazy Frog Song")
+print(f"My favourite songs are {','.join(favourite_songs)}") #4
+print("\n")
+
+book = []
+book.append("Harry Potter")
+book.append(" Ready Player One")
+book.append(" Green Eggs and Ham")
+book.append(" Maze Runner")
+book.append(" Star Wars Novels")
+print(','.join(book)) #5
+print("\n")
+"""
+
+pizza_toppings = []
+user_pt = input("Add a pizza topping\n")
+if user_pt != "":
+	pizza_toppings.append(user_pt)
+user_pt = input("Add a pizza topping\n")
+if user_pt != "":
+	pizza_toppings.append(user_pt)
+user_pt = input("Add a pizza topping\n")
+if user_pt != "":
+	pizza_toppings.append(user_pt)
+user_pt = input("Add a pizza topping\n")
+if user_pt != "":
+	pizza_toppings.append(user_pt)
+user_pt = input("Add a pizza topping\n")
+if user_pt != "":
+	pizza_toppings.append(user_pt)
+user_pt = input("Add a pizza topping\n")
+if user_pt != "":
+	pizza_toppings.append(user_pt)
+print(','.join(pizza_toppings))
