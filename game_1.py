43af58379caf3f0d78c817ebc119ed11ac6f05a2
riltho1 <100237088+riltho1@users.noreply.github.com>
2022-03-24 12:24:55 +1300

    Adventurelib.pt4

--- game_1.py
+++ game_1.py
@@ -1,4 +1,5 @@
 from adventurelib import *
+Room.items = Bag()
 
 #Room Descriptions
 space = Room("""
@@ -53,6 +54,28 @@ mess_hall.north = hallway
 bridge.south = escape_pods
 cargo.east = docking
 
+#Defining Items
+Item.description = "" #this adds a blank description to each item
+
+knife = Item("a dirty knife","knife")
+knife.description = "The knife has a dull sheen to it but it looks rather sharp."
+
+red_keycard = Item("a red keycard","keycard","red card","red card")
+red_keycard.description = "It's a red keycard. It probably opens a door or a locker."
+
+spacesuit = Item("spacesuit","old worn spacesuit")
+spaceship.description = "The spacesuit is quite old and worn out."
+
+music_player = Item("music player","apple music player")
+music_player.description = "There is an old Apple Music Player that hs some songs from the 2000s"
+
+#Defining Bags
+
+mess_hall.items.add(red_keycard)
+cargo.items.add(knife)
+docking.items.add(spacesuit)
+quaters.items.add(music_player)
+
 @when ("go DIRECTION")
 def travel (direction):
 	global current_room
@@ -65,7 +88,9 @@ def travel (direction):
 
 #Variable
 current_room = space
+invetory = Bag()
 
+#Binds
 @when("enter airlock")
 @when("enter spacehip")
 @when("enter ship")
@@ -83,6 +108,32 @@ def enter_spaceship():
 		""")
 
 @when("look")
+def look():
+	print(current_room)
+	print(f"There are exits in the directions {current_room.exits()}.")
+	if len(current_room.items) > 0: #If there are some items in the room
+		print("You also see:")
+		for item in current_room.items:
+			print(item)#print out out each item in the room
+
+@when("get ITEM")
+@when("take ITEM")
+@when("pick up ITEM")
+def pickup(item):
+	if item in current.room.items:
+		t = current_room.items.take(item)
+		inventory.add(t)
+		print(f"You pick up the {item}.")
+	else:
+		print(f"You don't see a {item}")
+
+@when("inventory")
+@when("show inventory")
+@when("what is in my pocket")
+def player_inventory():
+	print("You are carrying")
+	for item in inventory:
+		print(item)
 
 def main():
 	start()
